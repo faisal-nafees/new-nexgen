bfd7c6e99138b60cf648fa76d73dfd4f85a3ed3f
Faisal Nafees <mofaisal739@gmail.com>
2022-11-29 13:00:39 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,87 @@
+// function openForm() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+// }
+
+// Hero section slider
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: 'true',
+  fade: "true",
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Swiper Testimonial
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Navbar Toggle
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("header").addClass("black");
+  } else {
+    $("header").removeClass("black");
+  }
+});
+
+// Form
+
+// tailwind.config = {
+//   plugins: [require("@tailwindcss/forms")],
+// };
+
+
+  
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primaryColor: "#000",
+      secondaryColor: "#fcae04",
+      darkGray: "#1f2937",
+      lightGray: "#e5e7eb",
+      white: "#fff",
+      current: "currentColor",
+      gray_600: "#4b5563",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
